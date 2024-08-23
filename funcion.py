3dfe23ac9a8e9499822af17ccd144f19a6f1019f
DavidIza <david.izaguirre@udem.edu>
2024-08-23 17:15:09 -0600

    mejora
    Ahora puede hacer coseno tambien

--- funcion.py
+++ funcion.py
@@ -1,9 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-def plot_sin(x,y):
+
+def plot_sin(x):
+    y = np.sin(x)
+    plt.plot(x,y)
+def plot_cos(x):
+    y = np.cos(x)
     plt.plot(x,y)
 
 x =  np.arange(0,2*np.pi,0.001)
-y = np.sin(x)
-plot_sin(x,y)
+
+
+plot_sin(x)
+plot_cos(x)
 plt.show()
